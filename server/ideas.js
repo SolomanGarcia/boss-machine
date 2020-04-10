b726b39d122ace8fd9a7e4fc5439d531b45f0260
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-10 12:17:27 -0600

    add to ideas.js

--- server/ideas.js
+++ server/ideas.js
@@ -8,4 +8,17 @@ const {
   deleteFromDatabasebyId,
 } = require('./db');
 
+ideasRouter.param('id', (req, res, next, id) => {
+  const idea = getFromDatabaseById('ideas', id);
+  if (idea) {
+    req.idea = idea;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
+// GET /api/ideas to get an array of all ideas
+ideasRouter.get('/', )
+
 module.exports = ideasRouter;
