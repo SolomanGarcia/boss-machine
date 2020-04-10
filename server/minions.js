88d206b776d4263fac49ad36ff7e833ad6cc0725
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-10 11:55:03 -0600

    add route to GET single minion by id

--- server/minions.js
+++ server/minions.js
@@ -18,13 +18,40 @@ minionsRouter.param('minionId', (req, res, next, id) => {
   }
 });
 
+// // GET /api/minions to get an array of all minions
 minionsRouter.get('/', (req, res, next) => {
   res.send(getAllFromDatabase('minions'));
 });
 
+// POST /api/minions to create a new minion and save it to the database
 minionsRouter.post('/', (req, res, next) => {
   const newMinion = addToDatabase('minions', req.body);
   res.send(updateInstanceInDatabase);
 });
 
+// GET /api/minions/:minionId to get a single minion by id
+minionsRouter.get('/:minionId', (req, res, next) => {
+  res.send(req.minion)
+});
+
 module.exports = minionsRouter;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
