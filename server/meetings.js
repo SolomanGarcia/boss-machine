36c6b47b329ab07fc5477e291e7cfd74fff8c293
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-10 12:40:10 -0600

    add GET and POST routes in meetings.js

--- server/meetings.js
+++ server/meetings.js
@@ -2,6 +2,15 @@ const meetingsRouter = require('express').Router();
 
 const { getAllFromDatabase, addToDatabase, deleteAllFromDatabase, createMeeting } = require('./db');
 
+// GET /api/meetings to get an array of all meetings
+meetingsRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('meetings'));
+});
 
+// POST /api/meetings to create a new meeting and save it to the database
+meetingsRouter.post('/', (req, res, next) => {
+  let newMeeting = addToDatabase('meetings', createMeeting());
+  res.status(201).send(newMeeting);
+});
 
 module.exports = meetingsRouter;
