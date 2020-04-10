98fc98ad62e2240d0a068e05131835e5bb1ae486
Soloman Garcia <soloman.garcia@gmail.com>
2020-04-10 10:19:59 -0600

    add to minions.js

--- server/minions.js
+++ server/minions.js
@@ -8,4 +8,14 @@ const {
   deleteFromDatabasebyId,
 } = require("./db");
 
+minionsRouter.param('minionId', (req, res, next, id) => {
+  const minion = getFromDatabaseById('minions', id);
+  if (minion) {
+    req.minion = minion;
+    next();
+  } else {
+    res.status(404).send();
+  }
+});
+
 module.exports = minionsRouter;
